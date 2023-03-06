8e66917817fa0236fd7c68547ad204672237cc38
ABDULLAH KHALFAN ABDULLAH ALSHBLI <AKA486@KAHF.ORG>
2023-03-06 09:02:17 +0400

    new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment83';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onIntervalFired(fireedNumber: number) {
+    if(fireedNumber %2 === 0){
+      this.evenNumbers.push(fireedNumber);
+    }else{
+      this.oddNumbers.push(fireedNumber);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
+import {NgbModule} from"@ng-bootstrap/ng-bootstrap"; 
 import { AppComponent } from './app.component';
+import { EvenComponent } from './even/even.component';
+import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvenComponent,
+    GameControlComponent,
+    OddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
